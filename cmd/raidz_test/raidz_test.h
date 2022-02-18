f5b4a5fca231d18ba645ae5f2af24cab22a322d7
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-02-18 14:06:31 +0100

    cmd/raidz_test/raidz_test.h: Remove bcopy(), bzero(), bcmp()

--- cmd/raidz_test/raidz_test.h
+++ cmd/raidz_test/raidz_test.h
@@ -28,7 +28,7 @@
 
 #include <sys/spa.h>
 
-static const char *raidz_impl_names[] = {
+static const char *const raidz_impl_names[] = {
 	"original",
 	"scalar",
 	"sse2",
@@ -90,19 +90,19 @@ static inline size_t ilog2(size_t a)
 #define	D_INFO	1
 #define	D_DEBUG	2
 
-#define	LOG(lvl, a...)				\
+#define	LOG(lvl, ...)				\
 {						\
 	if (rto_opts.rto_v >= lvl)		\
-		(void) fprintf(stdout, a);	\
+		(void) fprintf(stdout, __VA_ARGS__);	\
 }						\
 
-#define	LOG_OPT(lvl, opt, a...)			\
+#define	LOG_OPT(lvl, opt, ...)			\
 {						\
 	if (opt->rto_v >= lvl)			\
-		(void) fprintf(stdout, a);	\
+		(void) fprintf(stdout, __VA_ARGS__);	\
 }						\
 
-#define	ERR(a...)	(void) fprintf(stderr, a)
+#define	ERR(...)	(void) fprintf(stderr, __VA_ARGS__)
 
 
 #define	DBLSEP "================\n"
